b5e3948d6ea44df2460c6801fa763f0988fe9303
陈浪 <chenlang@sx985.com>
2020-05-28 10:26:59 +0800

    修复boolean类型不能映射为null的bug

--- app/src/main/java/com/ellen/dhcsqlite/NewActivtiy.java
+++ app/src/main/java/com/ellen/dhcsqlite/NewActivtiy.java
@@ -59,9 +59,7 @@ public class NewActivtiy extends Activity {
 
             }
         });
-
-        Log.e("Ellen2018", "表是否存在:" + studentTable.isExist());
-
+        
         Student student = new Student("Ellen2018", 19, "18272167574", "火星");
         student.setMan(true);
         Father father = new Father("Ellen2019", "1", "尼玛");
