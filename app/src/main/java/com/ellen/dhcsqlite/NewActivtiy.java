c6721b97b688ffaed74532879343832c155b7492
陈浪 <chenlang@sx985.com>
2020-05-29 09:02:33 +0800

    fix 主键添加@DhcSqlFieldName的添加数据发生异常的bug

--- app/src/main/java/com/ellen/dhcsqlite/NewActivtiy.java
+++ app/src/main/java/com/ellen/dhcsqlite/NewActivtiy.java
@@ -124,9 +124,7 @@ public class NewActivtiy extends Activity {
         studentTable.saveData(studentList);
 
         studentTable.getAutoDesignOperate().update1("新的名字","Ellen2018_0");
-
-        String whereSql = Where.getInstance(false).addAndWhereValue("your_age", WhereSymbolEnum.MORE_THAN, 3).createSQL();
-
+      
         for (Student student1 : studentTable.getAllData()) {
             Log.e("Ellen2018", "数据:" + student1.toString());
         }
